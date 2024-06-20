611ffaa88485f6ca046e630c3c12bdd3a9cecc22
sultanaraziamst <sultanaraziaofficial@gmail.com>
2024-06-21 01:40:33 +0600

    firstcommit

---
+++ shakerology.py
@@ -0,0 +1,10 @@
+# Define cocktail data
+cocktail_data = {
+    "Margarita": ["2 oz Tequila", "1 oz Lime juice", "1 oz Cointreau", "Salt for rimming"],
+    "Martini": ["2 oz Gin", "1/2 oz Dry vermouth", "Lemon twist or olive for garnish"],
+    "Negroni": ["1 oz Gin", "1 oz Campari", "1 oz Sweet vermouth", "Orange twist or slice for garnish"],
+    "Old Fashioned": ["2 oz Bourbon or Rye whiskey", "1 sugar cube or 1/2 oz simple syrup", 
+                      "2-3 dashes Angostura bitters", "Orange twist for garnish"],
+    "Cosmopolitan": ["1 1/2 oz Vodka", "1 oz Cranberry juice", "1/2 oz Triple sec", "1/2 oz Lime juice"],
+}
+    # Add more cocktails here
